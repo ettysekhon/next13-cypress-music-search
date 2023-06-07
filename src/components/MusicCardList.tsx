165b5e956e6bb8e2e9bf3f95f367bd5030f4ec3a
Etty Sekhon <etty.sekhon@gmail.com>
2023-06-07 13:37:23 +0100

    fix: keep url encoding consistent

--- src/components/MusicCardList.tsx
+++ src/components/MusicCardList.tsx
@@ -2,8 +2,7 @@
 
 import { SearchResultsType } from '@/utils/types'
 import { MusicCard } from './MusicCard'
-import { useCallback } from 'react';
-import { usePathname, useSearchParams } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 
 type MusicCardListProps = {
@@ -16,16 +15,6 @@ export const MusicCardList = ({ results, term, pageNumber }: MusicCardListProps)
   // const [pageNumber, setPageNumber] = useState(1);
   // const [searchResults, setSearchResults] = useState<SearchResultsType[]>([]);
   const pathname = usePathname();
-  const searchParams = useSearchParams()!;
-
-  const createQueryString = useCallback(
-    (name: string, value: string) => {
-      const params = new URLSearchParams();
-      params.set(name, value);
-      return params.toString();
-    },
-    [searchParams],
-  );
 
   // NOTE: following a more client centric rendering approach like below preserves scroll
   // with Next13
@@ -43,8 +32,6 @@ export const MusicCardList = ({ results, term, pageNumber }: MusicCardListProps)
 
   // const loadMore = useCallback(() => setPageNumber(state => state + 1), [])
 
-  const link = `${pathname}?${createQueryString('term', term)}&${createQueryString('pageNumber', (pageNumber + 1).toString())}`
-
   return (
     <>
       <div data-testid="music-card-list" className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
@@ -57,7 +44,7 @@ export const MusicCardList = ({ results, term, pageNumber }: MusicCardListProps)
         data-testid="load-more"
         scroll={false}
         className="mt-4 w-24 h-24 bg-red-600 text-white text-sm font-medium rounded-full flex items-center justify-center hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
-        href={link}
+        href={`${pathname}?term=${encodeURI(term)}&pageNumber=${(pageNumber + 1).toString()}`}
       >
         Load More
       </Link >
